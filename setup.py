d6484e1322041843ec77a9250ee5a0d4f7ead57e
Yegor Pomortsev <yegor@pomortsev.com>
2021-07-05 21:41:06 -0700

    v0.0.2

--- setup.py
+++ setup.py
@@ -3,19 +3,16 @@ try:
 except ImportError:
     from distutils.core import setup
 
-
-setup(
-    name='ccss2edr',
-    version='0.0.1',
-    description='CCSS to EDR converter',
-    author='Yegor Pomortsev',
-    author_email='yegor@pomortsev.com',
-    url='http://pomortsev.com',
-    packages=['ccss2edr'],
-    entry_points={
-        'console_scripts': [
-            'ccss2edr = ccss2edr.ccss2edr:main',
-            'dumpedr = ccss2edr.dumpedr:main'
-        ]
-    }
-)
+setup(name='ccss2edr',
+      version='0.0.2',
+      description='CCSS to EDR converter',
+      author='Yegor Pomortsev',
+      author_email='yegor@pomortsev.com',
+      url='http://pomortsev.com',
+      packages=['ccss2edr'],
+      entry_points={
+          'console_scripts': [
+              'ccss2edr = ccss2edr.ccss2edr:main',
+              'dumpedr = ccss2edr.dumpedr:main'
+          ]
+      })
